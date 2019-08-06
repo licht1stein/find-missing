3a1287828c2e96f1013fd52788362bdc6124ad72
MB <mb@m1k.pw>
2019-08-06 12:41:34 +0300

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='find-missing',
+    version='',
+    packages=['tests', 'find_missing'],
+    url='',
+    license='',
+    author='MB',
+    author_email='',
+    description=''
+)
