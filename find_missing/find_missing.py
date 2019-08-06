a80bc55c0305dad9c0a99ef521206b1d83fef2b0
MB <mb@m1k.pw>
2019-08-06 13:36:55 +0300

    Added --verbose mode

--- find_missing/find_missing.py
+++ find_missing/find_missing.py
@@ -5,20 +5,28 @@ import click
 
 
 @click.command()
-@click.option("--exact/--not-exact", default=False)
+@click.option('-e/-p', "--exact/--partial", default=False)
+@click.option('-v/-s', '--verbose/--silent', default=False)
 @click.argument("list_of_files", type=str)
-def main(list_of_files, exact: bool):
+def main(list_of_files, exact: bool, verbose: bool):
     list_of_files = re.findall(r"[A-Za-z\d.\-_]+", list_of_files)
-    # click.echo(f"Looking for files: {list_of_files}")
+
+    if verbose:
+        click.echo(f"Looking for files: {list_of_files}. Exact mode: {exact}")
     cwd = Path.cwd()
 
     files_in_folder = [f for f in list(cwd.glob("*.*")) if f.is_file()]
-    # click.echo(f"Files in dir: {[f.name for f in files_in_folder]}")
+
+    if verbose:
+        click.echo(f"Files in dir: {[f.name for f in files_in_folder]}")
 
     if not exact:
         result = find_partial_match(list_of_files, files_in_folder)
     else:
         result = find_exact_match(list_of_files, files_in_folder)
+
+    if verbose:
+        click.echo(f'\nFound {len(result)} missing files:')
 
     if result:
         click.echo("\n".join(result))
